2ccd5d00fd475a93a0cbc4225cb4e81069cb10f5
Juan Cruz Miranda <juanmir227@gmail.com>
2024-01-17 20:56:20 -0300

    server and util working as intended

--- Portfolio Projects/Regression/server/server.py
+++ Portfolio Projects/Regression/server/server.py
@@ -1,19 +1,42 @@
 from flask import Flask, request, jsonify
 import util
 
-
 app = Flask(__name__)
-
-@app.route('/get_car_models')
-def get_car_models():
-    return
-
 
 
 @app.route('/hello')
 def hello():
-    return 'This is my page'
+    return 'Hi'
+
+
+@app.route('/get_car_models', methods =['GET'])
+def get_car_models():
+
+    response = jsonify({
+        'car_models': util.get_car_models()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_car_price', methods = ['POST'])
+def predict_car_price():
+
+    car_model = request.form['car_model']
+    year = int(request.form['year'])
+    km_driven = float(request.form['km_driven'])
+    fuel = request.form['fuel']
+    seller = request.form['seller']
+    transmission = request.form['transmission']
+    owner = request.form['owner']
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(car_model, year, km_driven, fuel, seller, transmission, owner)
+    })
+    
+    return response
 
 if __name__ == '__main__':
     print('Starting Python Flask server for used cars prediction...')
+    util.load_artifacts()
     app.run()
--- Portfolio Projects/Regression/server/util.py
+++ Portfolio Projects/Regression/server/util.py
@@ -1,11 +1,49 @@
 import json
+import pickle
+import numpy as np
 
 __car_models = None
 __data_columns = None
 __model = None
 
-def get_car_models():
-    pass
+def get_estimated_price(car_model, year, km_driven, fuel, seller, transmission, owner):
+    try:
+        car_model_loc_index = __data_columns.index('name_' + car_model.lower())
+    except:
+        car_model_loc_index = -1
+    try:
+        fuel_loc_index = __data_columns.index('fuel_' + fuel.lower())
+    except:
+        fuel_loc_index = -1
+    try:
+        seller_loc_index = __data_columns.index('seller_type_' + seller.lower())
+    except:
+        seller_loc_index = -1
+    try:
+        transmission_loc_index = __data_columns.index('transmission_' + transmission.lower())
+    except:
+        transmission_loc_index = -1
+    try:
+        owner_loc_index = __data_columns.index('owner_' + owner.lower())
+    except:
+        owner_loc_index = -1
+    
+    X = np.zeros(len(__data_columns))
+    X[0] = year
+    X[1] = km_driven
+
+    if car_model_loc_index >= 0:
+        X[car_model_loc_index] = 1
+    if fuel_loc_index >= 0:
+        X[fuel_loc_index] = 1
+    if seller_loc_index >= 0:
+        X[seller_loc_index] = 1
+    if transmission_loc_index >= 0:
+        X[transmission_loc_index] = 1
+    if owner_loc_index >= 0:
+        X[owner_loc_index] = 1
+    
+    return round(__model.predict([X])[0],2)
 
 
 def load_artifacts():
@@ -13,9 +51,22 @@ def load_artifacts():
     global __data_columns
     global __car_models
 
-    with open('.artifacts/columns.json', 'r') as f:
+    with open('./artifacts/columns.json', 'r') as f:
         __data_columns = json.load(f)['data_columns']
+        __car_models = __data_columns[13:]
+    global __model
+    with open('./artifacts/used_cars_model.pickle', 'rb') as f:
+        __model = pickle.load(f)
+    print('loading saved artifacts...done')
 
+def get_car_models():
+    return __car_models
+
+def get_data_columns():
+    return __data_columns
 
 if __name__ =='__main__':
-    print(get_car_models())
+    load_artifacts()
+    print(get_estimated_price('Maruti 800 AC', 2007, 70000, 'Petrol', 'Individual', 'Manual', 'First owner')) #real 833.0
+    # print(get_estimated_price('Hyundai Verna 1.6 SX', 2012, 100000, 'Diesel', 'Individual', 'Manual', 'First Owner')) #real 8333.0
+    # print(get_estimated_price('Hyundai i20 Magna 1.4 CRDi (Diesel)', 2014, 80000, 'Diesel', 'Individual', 'Manual', 'Second Owner')) #real 5694.0
